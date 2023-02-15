c50f74a34f8d4b9cdc952038e32c7d656aa7b597
PrangonGhose <prangonghose@gmail.com>
2023-02-16 05:05:58 +0600

    Add Router and Store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import store from './redux/configureStore';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
+  </React.StrictMode>,
 );
