8901c1236b915c3162dfec9cea900159fba3b6dc
PrangonGhose <prangonghose@gmail.com>
2023-02-16 17:10:08 +0600

    Change styling to meet guidelines

--- src/components/home.js
+++ src/components/home.js
@@ -8,26 +8,28 @@ const CountryCard = (props) => {
   } = props;
 
   return (
-    <div className="card country-card text-center border-danger mb-3 ms-2" style={{ width: '18rem' }}>
+    <div className="card country-card text-center border-danger mb-3 ms-2" style={{ width: '45%' }}>
       <Link to={`/weather/${countryCapital}`} className="link">
-        <div className="image">
-          <img src={countryFlag} alt={`Flag of ${countryFlag}`} className="card-img-top" />
-        </div>
-        <div className="card-body">
-          <h5 className="card-title">{countryName}</h5>
-          <p className="card-text text-start text-dark">
-            <strong>Continent: </strong>
-            {countryRegion}
-          </p>
-          <p className="card-text text-start text-dark">
-            <strong>Subcontinent: </strong>
-            {countrySubRegion}
-          </p>
-          <p className="card-text text-start text-dark">
-            <strong>Capital: </strong>
-            {countryCapital}
-          </p>
-          <p className="card-text text-center">View Present Weather</p>
+        <div className="row no-gutters">
+          <div className="col-md-8">
+            <img src={countryFlag} alt={`Flag of ${countryFlag}`} className="card-img-top" />
+          </div>
+          <div className="col-md-4 pt-4">
+            <h5 className="card-title text-white">{countryName}</h5>
+            <p className="card-text text-start text-white">
+              Continent:&nbsp;
+              {countryRegion}
+            </p>
+            <p className="card-text text-start text-white">
+              Subcontinent:&nbsp;
+              {countrySubRegion}
+            </p>
+            <p className="card-text text-start text-white">
+              Capital:&nbsp;
+              {countryCapital}
+            </p>
+            <p className="card-text text-center text-white">View Present Weather</p>
+          </div>
         </div>
       </Link>
     </div>
